6032c4bcf6b6ff87afd46bc7eef1a27c42003cf7
Sébastien Grison <74866413+SebGris@users.noreply.github.com>
2025-04-29 13:44:53 +0200

    Ajout de la méthode benefice_euros à la classe Action et mise à jour de la fonction load_actions_from_csv pour correspondre aux nouvelles colonnes du fichier CSV.

--- bruteforce.py
+++ bruteforce.py
@@ -8,19 +8,20 @@ class Action:
         self.cout = cout
         self.benefPourcent = benefPourcent
 
+    @property
+    def benefice_euros(self):
+        """
+        Calcule le bénéfice en euros basé sur le coût et le pourcentage de bénéfice.
+        """
+        return round(self.cout * (self.benefPourcent / 100), 2)
+
     def __str__(self):
-        return f"Action({self.nom}, {self.cout}, {self.benefPourcent})"
+        return f"Action({self.nom}, {self.cout}, {self.benefPourcent}, {self.benefice_euros})"
 
 
 def load_actions_from_csv(file_path):
     """
     Charge les actions à partir d'un fichier CSV.
-
-    Chaque ligne du fichier CSV est convertie en une instance de la classe Action.
-    Les colonnes attendues dans le fichier sont :
-    - 'Nom' : le nom de l'action
-    - 'Coût' : le coût de l'action (converti en entier)
-    - 'Bénéfice (%)' : le pourcentage de bénéfice (le symbole '%' est supprimé et converti en entier)
 
     :param file_path: Chemin vers le fichier CSV
     :return: Liste d'objets Action
@@ -28,15 +29,16 @@ def load_actions_from_csv(file_path):
     actions = []
     with open(file_path, mode='r', encoding='utf-8') as file:
         reader = csv.DictReader(file)
-        print("Contenu du fichier CSV :")
         for row in reader:
             # Création d'une instance de la classe Action pour chaque ligne
             # supprime le % de la colonne Bénéfice (%)
             # et convertit en entier
             action = Action(
-                nom=row['Nom'],
-                cout=int(row['Coût']),
-                benefPourcent=int(row['Bénéfice (%)'].replace('%', ''))
+                nom=row['Actions #'],
+                cout=int(row['Coût par action (en euros)']),
+                benefPourcent=int(
+                    row['Bénéfice (après 2 ans)'].replace('%', '')
+                )
             )
             # Ajout de l'action à la liste
             actions.append(action)
@@ -47,3 +49,8 @@ def load_actions_from_csv(file_path):
 csv_file = "Liste+d'actions+-+P7+Python+-+Feuille+1.csv"
 # Liste des actions
 actions = load_actions_from_csv(csv_file)
+
+# Affichage des actions
+print("Liste des actions :")
+for action in actions:
+    print(action)
